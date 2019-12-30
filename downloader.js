93b489936cd29ddd15d9698467b494f1a90101cf
michel lacle <michel@lacle.tech>
2019-12-30 08:55:19 -0400

    query image collection and get the original image

--- downloader.js
+++ downloader.js
@@ -2,24 +2,7 @@ let unirest = require('unirest');
 let https = require('https');
 let fs = require('fs');
 
-/**
- * Search the nasa images api and return the search result items' image collections
- */
-exports.searchAndGetResultItemsImageCollections = (url,  callback) => {
 
-    unirest.get(url).end(response => {
-
-        const imageCollections = [];
-
-        const items = response.body.collection.items;
-    
-        items.forEach(item => {
-            imageCollections.push(item.href)
-        });
-
-        callback(imageCollections);
-    });
-};
 
 function main() {
 
@@ -74,3 +57,38 @@ exports.downloadImageAndSaveToDisk = (imageUrl, filename) => {
         response.pipe(fileWriteStream);
     });
 };
+
+/**
+ * Search the nasa images api and return the search result items' image collections
+ */
+exports.searchAndGetResultItemsImageCollections = (url,  callback) => {
+
+    unirest.get(url).end(response => {
+
+        const imageCollections = [];
+
+        const items = response.body.collection.items;
+    
+        items.forEach(item => {
+            imageCollections.push(item.href)
+        });
+
+        callback(imageCollections);
+    });
+};
+
+exports.getOrigImageUrl = (imageCollectionUrl, callback) => {
+
+    unirest.get(imageCollectionUrl).end(response => {
+
+        const urls = response.body;
+
+        urls.forEach(url => {
+
+            // if we find the original we peace out immediately
+            if (url.match(/orig/)) {
+                callback(url);
+            }
+        })
+    });
+}
--- downloader.test.js
+++ downloader.test.js
@@ -7,13 +7,28 @@ test('that an image can be downloaded', () => {
     d.downloadImageAndSaveToDisk(imageUrl, 'file1.jpg');
 });
 
-test('that we can get the image collection for each search result', (done) => {
+test('that we can get the image collection urls for each search result', (done) => {
 
     const imageAPISearchUrl = 'https://images-api.nasa.gov/search?q=space&page=1&media_type=image&year_start=1920&year_end=2019';
 
     d.searchAndGetResultItemsImageCollections(imageAPISearchUrl, (items) => {
 
-        expect(items).not.toBeFalsy;
+        expect(items).not.toBeFalsy();
+        expect(items.length).toBeGreaterThan(0);
+        expect(items[0]).toMatch(/https:\/\/.*json/);
+        done();
+    });
+});
+
+test('that we can query the image collection and pick the original image', (done) => {
+
+    const collectionUrl = 'https://images-assets.nasa.gov/image/201211020022HQ/collection.json';
+
+    d.getOrigImageUrl(collectionUrl, (originalImageUrl) => {
+
+        console.log(originalImageUrl);
+        expect(originalImageUrl).not.toBeFalsy();
+        expect(originalImageUrl).toMatch(/http.*orig.*/);
         done();
     });
 });
