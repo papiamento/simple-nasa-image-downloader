f8d1976fd33e7a5124417392d1bdca11e5ca6d85
michel lacle <michel@lacle.tech>
2019-12-30 08:58:30 -0400

    clean up dead code

--- downloader.js
+++ downloader.js
@@ -2,61 +2,7 @@ let unirest = require('unirest');
 let https = require('https');
 let fs = require('fs');
 
-
-
-function main() {
-
-// here is the spec for "items"
-// https://images-api.nasa.gov/search?q=orion&page=1&media_type=image&year_start=1920&year_end=2019
-
-// each item contains links, data, a href
-// href contains url's to images of different sizes, including metadata
-
 let url = 'https://images-api.nasa.gov/search?q=space&page=1&media_type=image&year_start=1920&year_end=2019';
-
-let imageCollections = [];
-
-// TODO: SLOW HTTP CALL
-unirest.get(url).end(response => {
-
-    let items = response.body.collection.items;
-
-    items.forEach(item => {
-        imageCollections.push(item.href)
-    });
-
-    // TODO: BIG LOOP
-    imageCollections.forEach(imageCollection => {
-
-        // TODO: SLOW HTTP CALL
-        unirest.get(imageCollection).end(imageCollectionResponse => {
-
-            let urls = imageCollectionResponse.body
-
-            let imageUrls = [];
-            urls.forEach(url => {
-                if (url.match(/orig/)) {
-
-                    // TODO: for now we just test this in here, we need a testing utility
-                    downloadImageAndSaveToDisk(url, 'file1.jpg');
-                    imageUrls.push(url);
-                }
-            });
-
-            // TODO: SLOW HTTP CALL
-            console.log(imageUrls);
-        }); 
-    });
-});
-}
-
-exports.downloadImageAndSaveToDisk = (imageUrl, filename) => {
-    https.get(imageUrl, response => {
-
-        let fileWriteStream = fs.createWriteStream(filename);
-        response.pipe(fileWriteStream);
-    });
-};
 
 /**
  * Search the nasa images api and return the search result items' image collections
@@ -92,3 +38,11 @@ exports.getOrigImageUrl = (imageCollectionUrl, callback) => {
         })
     });
 }
+
+exports.downloadImageAndSaveToDisk = (imageUrl, filename) => {
+    https.get(imageUrl, response => {
+
+        let fileWriteStream = fs.createWriteStream(filename);
+        response.pipe(fileWriteStream);
+    });
+};
