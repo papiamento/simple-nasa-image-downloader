66767347cb3fe2ac2eab43453323b2ea1aef9907
michel lacle <michel@lacle.tech>
2020-01-02 15:12:29 -0500

    allowing cli to take search term

--- bin/download.js
+++ bin/download.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+function help() {
+  process.stdout.write('\n');
+  process.stdout.write('\n');
+
+  process.stdout.write('  Usage: nasa-images-download <search term>\n');
+  process.stdout.write('\n');
+
+  process.stdout.write('\n');
+}
+
+if (process.argv.length < 3) {
+  help();
+  process.exit(-1);
+}
+
+const searchTerm = process.argv[2];
+
+process.stdout.write(`\n... searching for "${searchTerm}" images and downloading ...\n\n`);
+
 const d = require('../downloader');
 
-d.downloadAllSearchedImages('venus', () => {});
+d.downloadAllSearchedImages(searchTerm, () => {});
